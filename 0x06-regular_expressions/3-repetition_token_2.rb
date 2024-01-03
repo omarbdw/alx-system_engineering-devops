0aa92e38468a569140ec212a3247f56128139ba2
omarbdw <omarbdw@yahoo.com>
2024-01-03 21:06:59 +0200

    test

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# The regular expression
-puts ARGV[0].scan(/hb*t*n/).join
+# The regular expression must be exactly matching to "hbtn, hbttn, hbtttn, hbttttn"
+puts ARGV[0].scan(/hbt{2,5}n/).join
