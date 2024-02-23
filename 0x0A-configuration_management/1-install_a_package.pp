c5adf917362190fda8ca94351aaef0f2ba92d44e
omarbdw <omarbdw@yahoo.com>
2024-02-23 21:49:49 +0200

    3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# Install Flask using pip3
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3',
+# puppet-lint
+exec { 'puppet-lint':
+command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
