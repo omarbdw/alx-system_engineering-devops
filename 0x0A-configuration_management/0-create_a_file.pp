7774fbe371df3b2271e066bfdb118f6920a58de6
omarbdw <omarbdw@yahoo.com>
2024-02-23 21:28:01 +0200

    0b

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Puppet manifest for creating a file in /tmp/school
 file { '/tmp/school':
-	ensure  => file,
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
+ensure  => file,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
