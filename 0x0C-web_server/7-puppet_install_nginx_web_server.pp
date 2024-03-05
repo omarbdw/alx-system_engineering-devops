ad7ab94061472996be7357a86695ac04e0b43bab
omarbdw <omarbdw@yahoo.com>
2024-03-05 10:38:33 +0200

    10.38

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,29 +1,33 @@
-# Setup New Ubuntu server with nginx
-# and add a custom HTTP header
-
+# Update the system using apt-get update
+# Install the nginx package
 exec { 'update system':
-        command => '/usr/bin/apt-get update',
+	command => '/usr/bin/apt-get update',
 }
 
+# Install the nginx package
 package { 'nginx':
 	ensure => 'installed',
 	require => Exec['update system']
 }
 
+# Create the index.html file with "Hello World!" content
 file {'/var/www/html/index.html':
 	content => 'Hello World!'
 }
 
+# Add a redirect rule to the nginx configuration file
 exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	command => 'sed -i "24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
+# Add an HTTP header to the nginx configuration file
 exec {'HTTP header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	command => 'sed -i "25i\    add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
+# Ensure the nginx service is running
 service {'nginx':
 	ensure => running,
 	require => Package['nginx']
