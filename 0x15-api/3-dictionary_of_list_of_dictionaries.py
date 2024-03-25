4f74c83c6928b0873179648e12e4d8dae0e4f3dd
omarbdw <omarbdw@yahoo.com>
2024-03-25 12:24:48 +0200

    3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
-"""Gather data from an API and export to CSV"""
-
+"""Gather data from an API and export to JSON"""
 import json
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    userId = sys.argv[1]
-    employee = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(userId),
-        timeout=5)
+    # Get all employees
+    employees = requests.get(
+        'https://jsonplaceholder.typicode.com/users', timeout=5)
+    employees = employees.json()
 
-    employeeName = employee.json().get('username')
-
+    # Get all todos
     todos = requests.get(
         'https://jsonplaceholder.typicode.com/todos', timeout=5)
+    todos = todos.json()
 
-    # Export data to JSON
+    # Create a dictionary of all employees and their tasks
     data = {}
-    for user in todos.json():
-        userId = user.get('userId')
-        task = {
-            "username": employeeName,
-            "task": user.get('title'),
-            "completed": user.get('completed')
-        }
-        if userId in data:
-            data[userId].append(task)
-        else:
-            data[userId] = [task]
+    for employee in employees:
+        userId = employee.get('id')
+        employeeName = employee.get('username')
+        user_tasks = []
+        for task in todos:
+            if task.get('userId') == userId:
+                task_data = {
+                    "username": employeeName,
+                    "task": task.get('title'),
+                    "completed": task.get('completed')
+                }
+                user_tasks.append(task_data)
+        data[userId] = user_tasks
 
+    # Save data to JSON file
     with open('todo_all_employees.json', 'w') as file:
         json.dump(data, file)
