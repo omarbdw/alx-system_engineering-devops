4179cff82d4d0c183e828b96c9c20a98e076c376
omarbdw <omarbdw@yahoo.com>
2024-04-13 15:05:51 +0200

    00

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,28 +1,6 @@
-# 0-strace_is_your_friend.pp
+# This is a simple example of how to use the exec resource to run a command on a node.
 
-# Install Apache package
-package { 'apache2':
-	ensure => installed,
-}
-
-# Configure Apache virtual host
-file { '/etc/apache2/sites-available/your_website.conf':
-	ensure  => present,
-	content => "# Your Apache virtual host configuration goes here",
-	require => Package['apache2'],
-}
-
-# Enable the virtual host
-exec { 'enable_virtual_host':
-	command     => 'a2ensite your_website.conf',
-	path        => '/usr/sbin:/usr/bin:/sbin:/bin',
-	refreshonly => true,
-	require     => File['/etc/apache2/sites-available/your_website.conf'],
-}
-
-# Restart Apache service
-service { 'apache2':
-	ensure    => running,
-	enable    => true,
-	subscribe => Exec['enable_virtual_host'],
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
